ed413d033e3db6b8d123791fd17e5a43ec7bd7c2
micha <michael.poludin@gmail.com>
2023-11-27 07:57:00 +0100

    CI test

--- src/NeuralCollaborativeFiltering.jl
+++ src/NeuralCollaborativeFiltering.jl
@@ -1,6 +1,6 @@
 module NeuralCollaborativeFiltering
 
-include("models\\dot_product_ncf.jl") # needs to be before train_model.jl
+include("models/dot_product_ncf.jl") # needs to be before train_model.jl
 
 include("train_model.jl")
 
